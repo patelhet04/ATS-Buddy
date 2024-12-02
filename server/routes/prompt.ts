3a5876359df6e3967abf23b2d02a9d3c67fe1b7d
patelhet04 <hetpatel.hp4199@gmail.com>
2024-12-02 16:45:52 -0500

    Added promptController to prompt route

--- server/routes/prompt.ts
+++ server/routes/prompt.ts
@@ -1,8 +1,9 @@
 import { FastifyPluginCallback } from "fastify";
+import { generate } from "../controller/promptController";
 // import { login, register } from "../controllers/authController";
 
 const promptRoutes: FastifyPluginCallback = (server, opts, done) => {
-  server.post("/generate", () => {});
+  server.post("/generate", generate);
   done();
 };
 
